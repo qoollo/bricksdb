a99de7691e4973de6ea725c5c6ddcd9d59d33e17
Waster <nikmenshov@mail.ru>
2015-06-29 18:41:56 +0300

    start rewriter transaction logic

---
+++ src/StorageSystem.Impl/Common/Data/DataTypes/DistributorData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Qoollo.Impl.Common.Data.DataTypes
+{
+    public class DistributorData
+    {
+        public class DistributorLock : IDisposable
+        {
+            private readonly DistributorData _data;
+
+            public DistributorLock(DistributorData data)
+            {
+                _data = data;
+                _data.Lock();
+            }
+
+            public void Dispose()
+            {
+                _data.Unlock();
+            }
+        }
+
+        public bool IsSyncAnswerSended { get; private set; }
+        public bool IsRollbackSended { get; private set; }
+
+        public DistributorData()
+        {
+            _lock = new ReaderWriterLockSlim();
+            IsSyncAnswerSended = false;
+            IsRollbackSended = false;
+        }
+
+        private readonly ReaderWriterLockSlim _lock;        
+
+        private void Lock()
+        {
+            _lock.EnterWriteLock();
+        }
+
+        private void Unlock()
+        {
+            _lock.ExitWriteLock();
+        }
+
+        public DistributorLock GetLock()
+        {
+            return new DistributorLock(this);
+        }
+
+        public void SendSyncAnswer()
+        {
+            IsRollbackSended = true;
+        }
+
+        public void SendRollback()
+        {
+            IsRollbackSended = true;
+        }
+    }
+}

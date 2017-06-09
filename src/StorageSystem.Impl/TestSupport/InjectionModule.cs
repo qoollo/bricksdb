845e7105289e9335db1ffcf63a31042ab1af66cd
nmenshov <nikmenshov@mail.ru>
2017-06-09 15:32:05 +0300

    add dp injection binding for net communication

---
+++ src/StorageSystem.Impl/TestSupport/InjectionModule.cs
@@ -0,0 +1,29 @@
+﻿using Ninject.Modules;
+using Qoollo.Impl.Modules.Net.ConnectionBehavior;
+using Qoollo.Impl.Modules.Net.ReceiveBehavior;
+using Qoollo.Impl.NetInterfaces.Distributor;
+using Qoollo.Impl.NetInterfaces.Proxy;
+using Qoollo.Impl.NetInterfaces.Writer;
+
+namespace Qoollo.Impl.TestSupport
+{
+    public class InjectionModule:NinjectModule
+    {
+        public override void Load()
+        {
+            BindClientAndServer<ICommonNetReceiverWriterForCollector>();
+            BindClientAndServer<ICommonNetReceiverWriterForWrite>();
+
+            BindClientAndServer<ICommonNetReceiverForDb>();
+            BindClientAndServer<ICommonNetReceiverForProxy>();
+
+            BindClientAndServer<ICommonProxyNetReceiver>();
+        }
+
+        private void BindClientAndServer<TType>()
+        {
+            Bind<IConnectionBehavior<TType>>().To<WpfConnection<TType>>();
+            Bind<IReceiveBehavior<TType>>().To<NetReceiveBehavior<TType>>();
+        }
+    }
+}

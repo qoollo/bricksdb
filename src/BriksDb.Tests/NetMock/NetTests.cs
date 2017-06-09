a632a00fc7155b9c444b5a4aa205225664b9d926
nmenshov <nikmenshov@mail.ru>
2017-06-09 15:10:10 +0300

    Add connection behavior and dp injection for client connection

---
+++ src/BriksDb.Tests/NetMock/NetTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using Ninject.Modules;
+using Ninject.Parameters;
+using Qoollo.Impl.Common.Server;
+using Qoollo.Impl.Modules.Net.ConnectionBehavior;
+using Qoollo.Impl.NetInterfaces;
+using Qoollo.Impl.NetInterfaces.Distributor;
+using Qoollo.Impl.Writer.WriterNet;
+
+namespace Qoollo.Tests.NetMock
+{
+    public class NModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IConnectionBehavior<ICommonNetReceiverForDb>>()
+            .To<WpfConnection<ICommonNetReceiverForDb>>();
+        }
+    }
+
+    [TestClass]
+    public class NetTests
+    {
+        [TestMethod]
+        public void SomeTest()
+        {
+            //var kernel = new StandardKernel();
+            //kernel.Load(Assembly.GetExecutingAssembly());
+
+            //kernel.Bind<ITest<object>>().To<Test2<object>>();
+
+            //var test = kernel.Get<ITest<object>>(new ConstructorArgument("t1", 10));
+            //kernel.Bind<IConnectionBehavior<ICommonNetReceiverForDb>>()
+            //    .To<WpfConnection<ICommonNetReceiverForDb>>();
+
+            var kernel = new StandardKernel();
+            var ass = Assembly.GetExecutingAssembly();
+            kernel.Load(Assembly.GetExecutingAssembly());            
+
+            var connection = kernel.Get<IConnectionBehavior<ICommonNetReceiverForDb>>(
+                new ConstructorArgument("server", new ServerId("123", 123)),
+                new ConstructorArgument("configuration", (object)null),
+                new ConstructorArgument("timeoutConfiguration", (object)null));
+            //var connection = new SingleConnectionToDistributor(new ServerId("123", 123), null, null);
+        }
+
+        
+
+        interface ITest<TType>
+        {
+             
+        }
+
+        private abstract class Test1<TType> : ITest<TType>
+        {
+        }
+
+        private class Test2<TType> : Test1<TType>
+        {
+            private readonly int _t1;
+
+            public Test2(int t1)
+            {
+                _t1 = t1;
+            }
+        }
+    }
+}
---
+++ src/StorageSystem.Impl/Modules/Net/ConnectionBehavior/IConnectionBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Qoollo.Impl.Common.Server;
+
+namespace Qoollo.Impl.Modules.Net.ConnectionBehavior
+{
+    internal interface IConnectionBehavior<TConnection>:IDisposable
+    {
+        ServerId Server { get; }
+
+        bool Connect();
+        void Dispose();
+
+        Task<TResult> SendAsyncFunc<TResult, TApi>(Func<TApi, Task<TResult>> func, Func<Exception, TResult> errorRet,
+            string errorLogFromData) where TApi : class;
+
+        TResult SendFunc<TResult, TApi>(Func<TApi, TResult> func, Func<Exception, TResult> errorRet,
+            string errorLogFromData) where TApi : class;
+    }
+}
